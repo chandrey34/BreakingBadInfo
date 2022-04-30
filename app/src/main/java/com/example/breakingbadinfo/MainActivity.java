7dd51ecb272e6c3f6d5a5ee022ecafdd47f35303
chandrey34 <vip034@ya.ru>
2022-05-01 00:28:40 +0300

    Fix MainActivity, activity_main

--- app/src/main/java/com/example/breakingbadinfo/MainActivity.java
+++ app/src/main/java/com/example/breakingbadinfo/MainActivity.java
@@ -14,95 +14,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //объявляем кнопки и присваеваем кнопкам ссылки
         Button series_btn = (Button) findViewById(R.id.series_btn);
         Button character_btn = (Button) findViewById(R.id.character_btn);
         Button quotes_btn = (Button) findViewById(R.id.quotes_btn);
         Button death_btn = (Button) findViewById(R.id.death_btn);
 
-        //обработчик события по нажатию на кнопки с главного экрана
-        View.OnClickListener onClickListener = new View.OnClickListener() {
+        //обработчик события по нажатию на кнопку Серии
+        series_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.series_btn:
-                        openSeriesActivity();
-                        break;
-                    case R.id.character_btn:
-                        openCharacterActivity();
-                        break;
-                    case R.id.quotes_btn:
-                        openQuotesActivity();
-                        break;
-                    case R.id.death_btn:
-                        openDeathActivity();
-                        break;
-                }
+                openActivity(view);
             }
-        };
-        series_btn.setOnClickListener(onClickListener);
-        character_btn.setOnClickListener(onClickListener);
-        quotes_btn.setOnClickListener(onClickListener);
-        death_btn.setOnClickListener(onClickListener);
+        });
+        //обработчик события по нажатию на кнопку Персонажи
+        character_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(view);
+            }
+        });
+        //обработчик события по нажатию на кнопку Цитаты
+        quotes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(view);
+            }
+        });
+        //обработчик события по нажатию на кнопку Смерти
+        death_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity(view);
+            }
+        });
     }
-
-//        //обработчик события по нажатию на кнопку Серии
-//        series_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openSeriesActivity();
-//                openCharacterActivity();
-//                openQuotesActivity();
-//                openDeathActivity();
-//            }
-//        });
-//        //обработчик события по нажатию на кнопку Персонажи
-//        character_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openCharacterActivity();
-//            }
-//        });
-//        //обработчик события по нажатию на кнопку Цитаты
-//        quotes_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openQuotesActivity();
-//            }
-//        });
-//        //обработчик события по нажатию на кнопку Смерти
-//        death_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openDeathActivity();
-//            }
-//        });
-//}
-
 
     //метод перехода на другой экран с Сериями
-    public void openSeriesActivity() {
-        Intent intent = new Intent(this, SeriesActivity.class);
-        startActivity(intent);
+    public void openActivity(View view) {
+        switch (view.getId()) {
+            case R.id.series_btn:
+                Intent intent = new Intent(MainActivity.this, SeriesActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.character_btn:
+                Intent intent1 = new Intent(MainActivity.this, CharacterActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.quotes_btn:
+                Intent intent2 = new Intent(this, QuotesActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.death_btn:
+                Intent intent3 = new Intent(this, DeathActivity.class);
+                startActivity(intent3);
+                break;
+        }
     }
-
-    public void openCharacterActivity() {
-        Intent intent = new Intent(this, CharacterActivity.class);
-        startActivity(intent);
-    }
-
-    public void openQuotesActivity() {
-        Intent intent = new Intent(this, QuotesActivity.class);
-        startActivity(intent);
-    }
-
-    public void openDeathActivity() {
-        Intent intent = new Intent(this, DeathActivity.class);
-        startActivity(intent);
-    }
-
-
 }
 
 
